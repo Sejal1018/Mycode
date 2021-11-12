736bf1d59c48acb025a177a174eb049db6450b07
Sejal1018 <72978789+Sejal1018@users.noreply.github.com>
2021-11-12 14:20:08 +0530

    create a Node class for creating the Node of SSL.

--- src/LinkedList/SinglyLinkedList.java
+++ src/LinkedList/SinglyLinkedList.java
@@ -1,4 +1,12 @@
 package LinkedList;
 
 public class SinglyLinkedList {
+    class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
 }
