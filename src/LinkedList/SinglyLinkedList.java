817dfaae3e1001e9b93878403c9c5317cf95d7be
Sejal1018 <72978789+Sejal1018@users.noreply.github.com>
2021-11-12 15:04:11 +0530

    write method to add newNode.

--- src/LinkedList/SinglyLinkedList.java
+++ src/LinkedList/SinglyLinkedList.java
@@ -1,12 +1,24 @@
 package LinkedList;
 
 public class SinglyLinkedList {
-    class Node{
+    static Node head;
+    //Node creation
+    static class Node{
         int data;
         Node next;
         public Node(int data){
             this.data=data;
             this.next=null;
         }
+    }
+    public static void insert(int data){
+        Node newNode = new Node(data);
+        if(head==null){
+            head=newNode;
+        }
+        while(head.next!=null){
+            head=head.next;
+        }
+        head.next=newNode;
     }
 }
